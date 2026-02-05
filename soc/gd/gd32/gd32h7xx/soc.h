9a70845b1f28e37e4c564402bb8ddcc03523a256
jianpingwu1 <jianping.wu@gigadevice.com>
2026-02-05 17:24:31 +0800

    soc: gd: add support for gd32h7xx (#17)
    Add initial support for the gd32h7xx SoC series,
    board, device tree, Kconfig files.
    
    Signed-off-by: Jianping Wu <jianping.wu@gigadevice.com>

---
+++ soc/gd/gd32/gd32h7xx/soc.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026 GigaDevice Semiconductor Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_ARM_GIGADEVICE_GD32H7XX_SOC_H_
+#define _SOC_ARM_GIGADEVICE_GD32H7XX_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <gd32h7xx.h>
+
+/* The GigaDevice HAL headers define this, but it conflicts with the Zephyr can.h */
+#undef CAN_MODE_NORMAL
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* _SOC_ARM_GIGADEVICE_GD32H7XX_SOC_H_ */
