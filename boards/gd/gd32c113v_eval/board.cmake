1d5bec1a699efcbf8e7132b2224669c7aad2911b
Yuanshuo Chen <yuanshuo.chen@gigadevice.com>
2026-09-07 14:21:36 +0800

    GD32C113: add tests overlay
    Including:
    - Improve GD32C113 tests
    
    Signed-off-by: Yuanshuo Chen <yuanshuo.chen@gigadevice.com>

--- boards/gd/gd32c113v_eval/board.cmake
+++ boards/gd/gd32c113v_eval/board.cmake
@@ -1,7 +1,11 @@
 # Copyright (c) 2026, GigaDevice Semiconductor Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(pyocd "--target=GD32C113VB" "--frequency=4000000" "--tool-opt=--pack=${ZEPHYR_HAL_GIGADEVICE_MODULE_DIR}/${CONFIG_SOC_SERIES}/support/GigaDevice.GD32C11x_DFP.1.4.0.pack")
+
+# GD32C11x series is not yet supported by SEGGER J-Link
 board_runner_args(jlink "--device=GD32C113VB" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
