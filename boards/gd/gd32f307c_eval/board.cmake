ea20c8020a4ce51fe074c2d03c5c8aef97d539eb
Junying Wang <junying.wang@gigadevice.com>
2026-09-18 14:50:27 +0800

    boards: gd: gd32f307c_eval: enable driver test support
    Add pyocd, jlink and gd32isp runner args, extend the supported list
    with spi, i2c and dma, and add gpio, spi, uart and flash test overlays.
    Loopback pins avoid the ENET-multiplexed ones, as the board populates
    the Ethernet PHY.
    
    Signed-off-by: Junying Wang <junying.wang@gigadevice.com>

--- boards/gd/gd32f307c_eval/board.cmake
+++ boards/gd/gd32f307c_eval/board.cmake
@@ -1,9 +1,12 @@
 # Copyright (c) 2026, GigaDevice Semiconductor Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(pyocd "--target=gd32f307vc" "--frequency=4000000" "--tool-opt=--pack=${ZEPHYR_HAL_GIGADEVICE_MODULE_DIR}/${CONFIG_SOC_SERIES}/support/GigaDevice.GD32F30x_DFP.2.6.0.pack")
+
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
-board_runner_args(jlink "--device=GD32F307VG" "--speed=4000")
-board_runner_args(gd32isp "--device=GD32F307VGT6")
+board_runner_args(jlink "--device=GD32F307VC" "--speed=4000")
+board_runner_args(gd32isp "--device=GD32F307VCT6")
 include(${ZEPHYR_BASE}/boards/common/gd32isp.board.cmake)
